e48e80040328fbdb45355ace3c137b9f867a2247
sebanimm <gks32114221@gmail.com>
2022-08-14 10:32:38 +0900

    2022-08-14 | Detail 부분 추가

---
+++ src/components/MovieDetail.js
@@ -0,0 +1,47 @@
+import React from "react";
+import PropTypes from "prop-types";
+import style from "./Detail.css";
+
+function MovieDetail({ url, year, rating, coverImg, title, summary, genres }) {
+  let summaryText = summary;
+  if (summaryText.length > 1034) {
+    summaryText = summaryText.substring(0, 1030) + "...";
+  }
+  return (
+    <div className="detailContainer">
+      <div className="imgContainer">
+        <img className="coverImg" src={coverImg} alt="cover img" />
+        <a href={url} className="btnGoSite">
+          Go To Site
+        </a>
+      </div>
+      <div className="detailInfo">
+        <h2 className="detailTitle">{title}</h2>
+        <ul className="detailGenres detail">
+          {genres
+            ? genres.map((g) => (
+                <li className="detailGenre" key={g}>
+                  {g}
+                </li>
+              ))
+            : null}
+        </ul>
+        <p className="detailYear detail">{year}</p>
+        <p className="detailRating detail">⭐ {rating}</p>
+        <p className="detailSummary">{summaryText}</p>
+      </div>
+    </div>
+  );
+}
+
+MovieDetail.propTypes = {
+  url: PropTypes.string.isRequired,
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string),
+  year: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired,
+};
+
+export default MovieDetail;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,18 +1,42 @@
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import MovieDetail from "../components/MovieDetail";
 
 function Detail() {
   const { id } = useParams();
-  const getMovie = async () => {
+  const [loading, setLoading] = useState(true);
+  const [movieInfo, setMovieInfo] = useState([]);
+
+  const getMovieInfo = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    setMovieInfo(json.data.movie);
+    setLoading(false);
   };
+
+  let nowUrl = window.location.pathname;
+
   useEffect(() => {
-    getMovie();
-  }, []);
-  return <h1>Detail</h1>;
+    setLoading(true);
+    getMovieInfo();
+  }, [nowUrl]);
+
+  return loading ? (
+    <div className="loadingWrapper">
+      <h1 className="loading">Loading....</h1>
+    </div>
+  ) : (
+    <MovieDetail
+      url={movieInfo.url}
+      year={movieInfo.year}
+      rating={movieInfo.rating}
+      coverImg={movieInfo.medium_cover_image}
+      title={movieInfo.title_long}
+      summary={movieInfo.description_full}
+      genres={movieInfo.genres}
+    />
+  );
 }
 
 export default Detail;
